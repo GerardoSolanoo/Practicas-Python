31fdd7457ddfc0dec6e9f6fad2bfb40ff8070bae
gerardosolanoo <gerardosolano.contact@gmail.com>
2023-05-16 13:34:04 -0600

    add exercise

---
+++ w1/helloworld.py
@@ -0,0 +1,8 @@
+# Author: Gerardo Solano
+# Date: May 11, 2023
+# Universidad Veracruzana
+# helloworld.py
+
+print ("Hello world python")
+print ("Hello universe ...")
+print ("Welcome to python !!")
